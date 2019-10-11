d64f2d6a777690ae0f6ed4a130b86cc58d6c81a0
Kaskela Ronja <s1900620@edu.bc.fi>
2019-10-11 10:18:20 +0300

    Updates

---
+++ precache-manifest.c2b8d55bf4adeae16f17f861aefa52fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f278ae86bf39c3ee2cfbee643f1111b3",
+    "url": "/speed_game/index.html"
+  },
+  {
+    "revision": "5ebc2c52a8d724752c81",
+    "url": "/speed_game/static/css/main.1497d2f2.chunk.css"
+  },
+  {
+    "revision": "62905279d35c83f5200b",
+    "url": "/speed_game/static/js/2.fdf02958.chunk.js"
+  },
+  {
+    "revision": "5ebc2c52a8d724752c81",
+    "url": "/speed_game/static/js/main.1b574fe6.chunk.js"
+  },
+  {
+    "revision": "7deb69e0b3dc20e12e70",
+    "url": "/speed_game/static/js/runtime~main.3c5a8f43.js"
+  }
+]);
